554c7d65fb0169b50c48a01edd36c8ee612bb267
Nick Rimmer <nick.rimmer.2009@gmail.com>
2022-03-03 11:20:46 +0000

    Updated

--- Random-YouTube.py
+++ Random-YouTube.py
@@ -13,7 +13,7 @@ access_token_secret = os.environ['TWIT_ACC_SEC']
 # Configure Tweepy to access Twitter
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 #auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 try:
     api.verify_credentials()
     print('Successful Authentication')
